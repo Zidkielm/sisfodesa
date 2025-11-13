c8b2e5da5dbdbb059f5a3e359f1bdb6d22d1c350
Zidkielm <santoniozons@gmail.com>
2025-11-13 12:37:24 +0700

    fixing variable store data

--- app/Http/Controllers/ResidentController.php
+++ app/Http/Controllers/ResidentController.php
@@ -26,7 +26,7 @@ class ResidentController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $validatedData = $request->validate([
             'nik' => ['required', 'min:16', 'max:16'],
             'name' => ['required', 'max:100'],
             'gender' => ['required', Rule::in(['male', 'female'])],
@@ -40,7 +40,7 @@ class ResidentController extends Controller
             'status' => ['required', Rule::in(['active', 'moved', 'deceased'])],
         ]);
 
-        Resident::create($request->validated());
+        Resident::create($validatedData);
 
         return redirect('/resident')->with('success', 'berhasil menambahkan data');
     }
