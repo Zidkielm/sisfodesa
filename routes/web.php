6f44dff686ec33ed7ef16b8bd62c04ec46eb5795
Zidkielm <santoniozons@gmail.com>
2025-11-21 14:38:43 +0700

    change route to login authentication

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ResidentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//Auth
+Route::get('/', [AuthController::class, 'login']);
+Route::post('/', [AuthController::class, 'authenticate']);
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
